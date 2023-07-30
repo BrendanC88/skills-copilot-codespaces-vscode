3d4d78ea35fde7e8e4a86090d63de4a16a5edccf
BrendanC88 <brdncoughlan@gmail.com>
2023-07-30 15:44:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,33 @@
-// create web server
+//Create web server
 const express = require('express');
 const app = express();
-const PORT = 3000;
-// create a router
-const commentRouter = require('./routes/comments');
-// create a path to the router
-app.use('/comments', commentRouter);
-// start the server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const port = 3000;
+//Create database client
+const MongoClient = require('mongodb').MongoClient;
+//Connect to database
+var db;
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('comments'); // whatever your database name is
+    //Start web server
+    app.listen(port, () => {
+        console.log('listening on ' + port);
+    });
 });
+
+//Create route for getting all comments
+app.get('/comments', (req, res) => {
+    db.collection('comments').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        res.send(result);
+    });
+});
+
+//Create route for posting comments
+app.post('/comments', (req, res) => {
+    db.collection('comments').save(req.body, (err, result) => {
+        if (err) return console.log(err);
+        console.log('saved to database');
+        res.redirect('/');
+    })
+})
