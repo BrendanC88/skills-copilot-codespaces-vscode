1f1294ed7652e16120a91c317703210850d39023
BrendanC88 <brdncoughlan@gmail.com>
2023-07-30 15:31:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberBox = document.getElementById("memberBox");
+    var skillsBox = document.getElementById("skillsBox");
+    var projectsBox = document.getElementById("projectsBox");
+    var contactBox = document.getElementById("contactBox");
+    var aboutBox = document.getElementById("aboutBox");
+    var homeBox = document.getElementById("homeBox");
+    member.style.backgroundColor = "#1abc9c";
+    skills.style.backgroundColor = "#16a085";
+    projects.style.backgroundColor = "#16a085";
+    contact.style.backgroundColor = "#16a085";
+    about.style.backgroundColor = "#16a085";
+    home.style.backgroundColor = "#16a085";
+    memberBox.style.display = "block";
+    skillsBox.style.display = "none";
+    projectsBox.style.display = "none";
+    contactBox.style.display = "none";
+    aboutBox.style.display = "none";
+    homeBox.style.display = "none";
+}
